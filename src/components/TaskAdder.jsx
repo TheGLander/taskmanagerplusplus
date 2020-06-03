0d6537b47acfd6104ad21ba39b0292c32caeb1b3
Lubomir <lubomir.yudenko@gmail.com>
2020-06-03 15:47:11 +0300

    feat(TaskAdder): Move task adder to onSubmit

--- src/components/TaskAdder.jsx
+++ src/components/TaskAdder.jsx
@@ -1,5 +1,4 @@
 import React from "react"
-import keycode from "keycode"
 import styles from "../styles"
 import { newTask } from "../redux/actions"
 import { connect } from "react-redux"
@@ -8,53 +7,34 @@ class TaskAdder extends React.Component {
 		super(props)
 		this.state = {
 			text: "",
-			taskList: props.taskList
 		}
 		this.handleChange = this.handleChange.bind(this)
+		this.appendTask = this.appendTask.bind(this)
 	}
-	appendTask(task) {
-		this.props.newTask(task)
+	appendTask(event) {
+		event.preventDefault()
+		this.props.newTask(this.state.text)
 		this.setState({ text: "" })
 	}
 	handleChange(e) {
 		this.setState({ text: e.target.value })
 	}
 	render() {
-		let input = (
-			<input
-				style={styles.inline}
-				type="text"
-				placeholder="Enter tasks here"
-				onKeyUp={e => {
-					if (keycode(e) === "enter") {
-						this.appendTask(input.props.value)
-					}
-				}}
-				value={this.state.text}
-				onChange={this.handleChange}
-			/>
-		)
-		this.input = input
-		let addButton = (
-			<button
-				style={styles.inline}
-				onClick={() => {
-					this.appendTask(input.props.value)
-				}}
-			>
-				Add task
-			</button>
-		)
-
 		return (
-			<div>
-				{input}
-				{addButton}
-			</div>
+			<form onSubmit={this.appendTask}>
+				<input
+					style={styles.inline}
+					type="text"
+					placeholder="Enter tasks here"
+					value={this.state.text}
+					onChange={this.handleChange}
+				/>
+				<button style={styles.inline}>Add task</button>
+			</form>
 		)
 	}
 }
-const mapDispatchToProps = dispatch => ({
-	newTask: name => dispatch(newTask(name))
+const mapDispatchToProps = (dispatch) => ({
+	newTask: (name) => dispatch(newTask(name)),
 })
 export default connect(() => ({}), mapDispatchToProps)(TaskAdder)
